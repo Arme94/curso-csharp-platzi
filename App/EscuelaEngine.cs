95f0bd2f47ecc4a69c0b3339f14dcc18970dc2e8
arnol.meneses <arnol.meneses@siesa.com>
2022-08-09 11:24:08 -0500

    first commit

---
+++ App/EscuelaEngine.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoreEscuela.Entidades;
+
+namespace CoreEscuela
+{
+    public sealed class EscuelaEngine
+    {
+        public Escuela Escuela { get; set; }
+
+        public EscuelaEngine()
+        {
+
+        }
+
+        public void Inicializar()
+        {
+            Escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria,
+            ciudad: "Bogotá", pais: "Colombia"
+            );
+
+            CargarCursos();
+            CargarAsignaturas();
+            CargarEvaluaciones();
+
+        }
+
+        public List<ObjetoEscuelaBase> GetObjetosEscuela(
+            out int conteoEvaluaciones,
+            out int conteoCursos,
+            out int conteoAsignaturas,
+            out int conteoAlumnos,
+            bool traeEvaluaciones = true,
+            bool traerAlumnos = true,
+            bool traerAsignaturas = true,
+            bool traerCursos = true)
+        {
+            conteoEvaluaciones = conteoAsignaturas = conteoAlumnos = 0;
+
+            var listaObj = new List<ObjetoEscuelaBase>();
+            listaObj.Add(Escuela);
+
+            if(traerCursos){
+                listaObj.AddRange(Escuela.Cursos);
+            }
+
+            conteoCursos = Escuela.Cursos.Count;
+            foreach (var curso in Escuela.Cursos)
+            {                   
+                conteoAsignaturas += curso.Asignaturas.Count;
+                conteoAlumnos += curso.Alumnos.Count;
+                if(traerAsignaturas){
+                    listaObj.AddRange(curso.Asignaturas);
+                }
+                if(traerAlumnos){
+                    listaObj.AddRange(curso.Alumnos);
+                }
+
+                if(traeEvaluaciones){
+                    foreach (var alumno in curso.Alumnos)
+                    {
+                        listaObj.AddRange(alumno.Evaluaciones);
+                        conteoEvaluaciones += alumno.Evaluaciones.Count;
+                    }
+                }
+            }
+
+            return listaObj;
+        }        
+
+        #region Métodos de Carga
+        private void CargarEvaluaciones()
+        {
+
+            foreach (var curso in Escuela.Cursos)
+            {
+                foreach (var asignatura in curso.Asignaturas)
+                {
+                    foreach (var alumno in curso.Alumnos)
+                    {
+                        var rnd = new Random(System.Environment.TickCount);
+
+                        for (int i = 0; i < 5; i++)
+                        {
+                            var ev = new Evaluación
+                            {
+                                Asignatura = asignatura,
+                                Nombre = $"{asignatura.Nombre} Ev#{i + 1}",
+                                Nota = (float)(5 * rnd.NextDouble()),
+                                Alumno = alumno
+                            };
+                            alumno.Evaluaciones.Add(ev);
+                        }
+                    }
+                }
+            }
+
+        }
+
+
+
+        private void CargarAsignaturas()
+        {
+            foreach (var curso in Escuela.Cursos)
+            {
+                var listaAsignaturas = new List<Asignatura>(){
+                            new Asignatura{Nombre="Matemáticas"} ,
+                            new Asignatura{Nombre="Educación Física"},
+                            new Asignatura{Nombre="Castellano"},
+                            new Asignatura{Nombre="Ciencias Naturales"}
+                };
+                curso.Asignaturas = listaAsignaturas;
+            }
+        }
+
+        private List<Alumno> GenerarAlumnosAlAzar(int cantidad)
+        {
+            string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+
+            var listaAlumnos = from n1 in nombre1
+                               from n2 in nombre2
+                               from a1 in apellido1
+                               select new Alumno { Nombre = $"{n1} {n2} {a1}" };
+
+            return listaAlumnos.OrderBy((al) => al.UniqueId).Take(cantidad).ToList();
+        }
+
+        private void CargarCursos()
+        {
+            Escuela.Cursos = new List<Curso>(){
+                        new Curso(){ Nombre = "101", Jornada = TiposJornada.Mañana },
+                        new Curso() {Nombre = "201", Jornada = TiposJornada.Mañana},
+                        new Curso{Nombre = "301", Jornada = TiposJornada.Mañana},
+                        new Curso(){ Nombre = "401", Jornada = TiposJornada.Tarde },
+                        new Curso() {Nombre = "501", Jornada = TiposJornada.Tarde},
+            };
+
+            Random rnd = new Random();
+            foreach (var c in Escuela.Cursos)
+            {
+                int cantRandom = rnd.Next(5, 20);
+                c.Alumnos = GenerarAlumnosAlAzar(cantRandom);
+            }
+        }
+    }
+    #endregion
+}
